8c2a3a11662ca816f83415be4152fd0273320a2c
Mohamed Habshey <Mohamed.habshey10@gmail.com>
2017-12-05 17:54:27 +0200

    APi Service

--- www/js/services.js
+++ www/js/services.js
@@ -1,9 +1,86 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+  .factory('apiService', ['$http', function ($http) {
+    var baseUrl = "/api/";
+    function sendReq(req) {
+      return $http(req)
 
-}])
+    }
+    return {
+      signUpAdv: function (AdvData) {
+        var req = {
+          method: 'POST',
+          url: baseUrl + 'advSignUp',
 
-.service('BlankService', [function(){
+          headers: {
+            'Content-Type': "application/json"
+          },
+          data: { "AdvData": JSON.stringify(AdvData) }
+        }
 
-}]);
+        return sendReq(req);
+
+      },
+
+      loginAdv: function (AdvData) {
+
+        var req = {
+          method: 'POST',
+          url: baseUrl + 'advLogin',
+          headers: {
+            'Content-Type': "application/json"
+          },
+          data: { "AdvData": JSON.stringify(AdvData) }
+        }
+
+        return sendReq(req);
+      },
+      getPlanTiers: function () {
+        var req = {
+          method: 'POST',
+          url: '/ordering/api/getPlanTiers',
+          headers: {
+            'Content-Type': "application/json"
+          }
+
+        }
+
+        return sendReq(req);
+      },
+      getPlans: function () {
+        var req = {
+          method: 'POST',
+          url: '/ordering/api/getPlans',
+          headers: {
+            'Content-Type': "application/json"
+          }
+
+        }
+
+        return sendReq(req);
+      },
+      reCaptcha: function () {
+        var req = {
+          method: 'POST',
+          url: '/ordering/api/fetchReCaptcha',
+          headers: {
+            'Content-Type': "application/json"
+          }
+
+        }
+
+        return sendReq(req);
+      }
+
+
+
+
+
+    }
+
+
+  }])
+
+  .service('BlankService', [function () {
+
+  }]);
