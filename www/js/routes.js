edeef594d9667f3bd58b3511d95b192bc47334fb
Mohamed Habashy <mohamed.habshey10@gmail.com>
2017-12-06 03:48:17 +0200

    align all title to center

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,7 +1,7 @@
 angular.module('app.routes', [])
 
-  .config(function ($stateProvider, $urlRouterProvider) {
-
+  .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+    $ionicConfigProvider.navBar.alignTitle('center')
     // Ionic uses AngularUI Router which uses the concept of states
     // Learn more here: https://github.com/angular-ui/ui-router
     // Set up the various states which the app can be in.
